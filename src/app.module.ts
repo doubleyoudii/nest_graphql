765e75c498c342595af09f694c0861296959c6bc
William Ducta <wducta@yondu.com>
2022-03-17 14:26:33 +0800

    CHORE: Generate Lesson Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver } from '@nestjs/apollo';
+import { LessonModule } from './lesson/lesson.module';
 @Module({
   imports: [
     GraphQLModule.forRoot({
       driver: ApolloDriver,
       autoSchemaFile: true,
     }),
+    LessonModule,
   ],
   controllers: [],
   providers: [],
