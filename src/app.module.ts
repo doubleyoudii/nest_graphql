2384a976defde4194cc4176431df9960177d55a5
William Ducta <wducta@yondu.com>
2022-03-21 07:35:32 +0800

    CHORE: Import mongodb with typeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,17 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver } from '@nestjs/apollo';
 import { LessonModule } from './lesson/lesson.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Lesson } from './lesson/lesson.entity';
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: 'mongodb://localhost/school-mangement',
+      syncronize: true,
+      useUnifiedTopology: true,
+      entities: [Lesson],
+    }),
     GraphQLModule.forRoot({
       driver: ApolloDriver,
       autoSchemaFile: true,
