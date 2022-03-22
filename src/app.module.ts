8f72c5b0e78cb91f2468bec037be313f796451ff
William Ducta <wducta@yondu.com>
2022-03-22 11:12:40 +0800

    CHORE: Import Required items in modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { LessonModule } from './lesson/lesson.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Lesson } from './lesson/lesson.entity';
 import { StudentModule } from './student/student.module';
+import { Student } from './student/student.entity';
 @Module({
   imports: [
     TypeOrmModule.forRoot({
@@ -12,7 +13,7 @@ import { StudentModule } from './student/student.module';
       url: 'mongodb://localhost/school-mangement',
       syncronize: true,
       useUnifiedTopology: true,
-      entities: [Lesson],
+      entities: [Lesson, Student],
     }),
     GraphQLModule.forRoot({
       driver: ApolloDriver,
