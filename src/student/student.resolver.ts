c00dfa7fc0d90c17995cdd03fbef86e377541bb9
William Ducta <wducta@yondu.com>
2022-03-22 11:56:39 +0800

    CHORE: Add Create Student and Get Students Method

--- src/student/student.resolver.ts
+++ src/student/student.resolver.ts
@@ -1,5 +1,21 @@
-import { Resolver } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
+import { CreateStudentInput } from './input/create-student.input';
+import { StudentService } from './student.service';
 import { StudentType } from './student.type';
 
 @Resolver((of) => StudentType)
-export class StudentResolver {}
+export class StudentResolver {
+  constructor(private studentService: StudentService) {}
+
+  @Query((returns) => [StudentType])
+  students() {
+    return this.studentService.getStudents();
+  }
+
+  @Mutation((returns) => StudentType)
+  createStudent(
+    @Args('studentCreateInput') studentCreateInput: CreateStudentInput,
+  ) {
+    return this.studentService.createStudent(studentCreateInput);
+  }
+}
--- src/student/student.service.ts
+++ src/student/student.service.ts
@@ -1,12 +1,28 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { CreateStudentInput } from './input/create-student.input';
 import { Student } from './student.entity';
+import { v4 } from 'uuid';
 
 @Injectable()
 export class StudentService {
   constructor(
     @InjectRepository(Student)
-    private studentService: Repository<Student>,
+    private studentRepository: Repository<Student>,
   ) {}
+
+  async getStudents(): Promise<Student[]> {
+    return await this.studentRepository.find();
+  }
+
+  async createStudent(createStudent: CreateStudentInput): Promise<Student> {
+    const { firstName, lastName } = createStudent;
+    const student = await this.studentRepository.create({
+      id: v4(),
+      lastName,
+      firstName,
+    });
+    return await this.studentRepository.save(student);
+  }
 }
