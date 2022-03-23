1de0e179b3597120bb62e8df8a29fb94b55fe6dc
William Ducta <wducta@yondu.com>
2022-03-23 13:13:46 +0800

    CHORE: Add Resolve field for Parents under lesson

--- src/lesson/lesson.resolver.ts
+++ src/lesson/lesson.resolver.ts
@@ -1,13 +1,25 @@
-import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
+import {
+  Resolver,
+  Query,
+  Mutation,
+  Args,
+  ResolveField,
+  Parent,
+} from '@nestjs/graphql';
+import { StudentService } from 'src/student/student.service';
 import { AssignStudentInput } from './input/assign-student.input';
 import { CreateLessonInput } from './input/create-lesson.input';
+import { Lesson } from './lesson.entity';
 import { LessonService } from './lesson.service';
 import { LessonType } from './lesson.type';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 @Resolver((of) => LessonType)
 export class LessonResolver {
-  constructor(private lessonService: LessonService) {}
+  constructor(
+    private lessonService: LessonService,
+    private studentService: StudentService,
+  ) {}
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   @Query((returns) => LessonType)
   lesson(@Args('id') id: string) {
@@ -32,5 +44,10 @@ export class LessonResolver {
     @Args('studentAssign') studentAssign: AssignStudentInput,
   ) {
     return this.lessonService.assignedStudentToLesson(studentAssign);
+  }
+
+  @ResolveField()
+  async students(@Parent() lesson: Lesson) {
+    return this.studentService.findStudents(lesson.students);
   }
 }
